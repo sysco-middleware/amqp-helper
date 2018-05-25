c080bfef67b8b7021d77395cc07417d9bad5d680
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-05-25 17:08:20 +0200

    add json protocol

---
+++ src/main/scala/no/sysco/middleware/amqphelper/utils/JsonRabbitCmdProtocol.scala
@@ -0,0 +1,32 @@
+package no.sysco.middleware.amqphelper.utils
+
+
+import spray.json.{DefaultJsonProtocol, JsonParser, RootJsonFormat}
+
+import scala.io.Source
+
+
+final case class Commands(queuesCmd: Option[Seq[QueueCmd]],
+                          exchangesCmd: Option[Seq[ExchangeCmd]],
+                          bindings: Option[Seq[BindCmd]])
+final case class QueueCmd(queue: String,
+                          durable: Boolean=false,
+                          exclusive: Boolean=false,
+                          autoDelete: Boolean=false)
+final case class ExchangeCmd(exchange: String, exchangeType: String)
+final case class BindCmd(queue: String, exchange: String, routingKey: String)
+
+trait JsonRabbitCmdProtocol extends DefaultJsonProtocol {
+  implicit def commandsFormat:RootJsonFormat[Commands] = jsonFormat(Commands, "queues", "exchanges", "bindings")
+  implicit def queueCmd:RootJsonFormat[QueueCmd] = jsonFormat(QueueCmd, "queue", "durable", "exclusive", "autoDelete")
+  implicit def exchangeCmdFormat:RootJsonFormat[ExchangeCmd] = jsonFormat(ExchangeCmd, "exchange", "type")
+  implicit def bindCmdFormat:RootJsonFormat[BindCmd] = jsonFormat(BindCmd, "queue", "exchange", "routingKey")
+}
+
+object RabbitCmds extends JsonRabbitCmdProtocol {
+  def load():Commands = {
+    val cmds = JsonParser(Source.fromFile("./rabbit-commands.json").getLines.mkString).convertTo[Commands]
+    // TODO: validation
+    cmds
+  }
+}
