094efb297da4893910755538b2a63e2b977e1aa2
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-06-05 21:34:15 +0200

    cmds validation

--- src/main/scala/no/sysco/middleware/amqphelper/utils/JsonRabbitCmdProtocol.scala
+++ src/main/scala/no/sysco/middleware/amqphelper/utils/JsonRabbitCmdProtocol.scala
@@ -32,14 +32,38 @@ trait JsonRabbitCmdProtocol extends DefaultJsonProtocol {
 object RabbitCmds extends JsonRabbitCmdProtocol {
   def load(): RabbitMqCommands = {
     val cmds = JsonParser(Source.fromFile("./rabbit-commands.json").getLines.mkString).convertTo[RabbitMqCommands]
-
-    /** TODO: validation
-      *
-      * 1. BuiltinExchangeType
-      * 2. RegEx
-      * 3. bindings queues&exchanges exists
-      *
-      * */
+    validate(cmds)
     cmds
+  }
+
+  private def validate(cmds: RabbitMqCommands) = {
+    import com.rabbitmq.client.BuiltinExchangeType._
+    val exchangeTypes = Seq(TOPIC,DIRECT,FANOUT,HEADERS).map(_.getType)
+
+    // cmds
+    val queueCmds = cmds.queuesCmd.getOrElse(Seq())
+    val exchangeCmds = cmds.exchangesCmd.getOrElse(Seq())
+    val bindCmds = cmds.bindings.getOrElse(Seq())
+
+    // seq names
+    val queueCmdNames = queueCmds.map(_.queue)
+    val exchangeCmdNames = exchangeCmds.map(_.exchange)
+
+    //    1 validate queues
+    queueCmds.foreach(qCmd => if(qCmd.queue.trim.isEmpty) throw new IllegalArgumentException("Queue name is empty"))
+
+    //    2 validate exchanges
+    exchangeCmds.foreach(eCmd => {
+      // exchange can be empty (default)
+      //      if(eCmd.exchange.trim.isEmpty) throw new IllegalArgumentException("Exchange name is empty")
+      if( ! exchangeTypes.contains(eCmd.exchangeType.toLowerCase.trim) ) throw new IllegalArgumentException("Incorrect exchange type")
+    })
+    //    3 validate bindings
+    bindCmds.foreach(bCmd => {
+      if(bCmd.routingKey.trim.isEmpty) throw new IllegalArgumentException("Bindings: routing key is empty")
+      if(bCmd.queue.trim.isEmpty) throw new IllegalArgumentException("Bindings: queue is empty")
+      if( ! queueCmdNames.contains(bCmd.queue) ) throw new IllegalArgumentException("Bindings: queue for binding can not be found in queue commands")
+      if( ! exchangeCmdNames.contains(bCmd.exchange) ) throw new IllegalArgumentException("Bindings: exchange for binding can not be found in exchange commands")
+    })
   }
 }
